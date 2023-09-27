49c73278c267c0c2bcedae0c38d3c04db0fee476
jongheonleee <qwefghnm1212@gmail.com>
2023-09-27 19:44:31 +0900

    스프링 웹 기본 흐름 구성

---
+++ src/main/java/hello/advanced/v0/OrderRepositoryV0.java
@@ -0,0 +1,29 @@
+package hello.advanced.v0;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+
+    public void save(String itemId) {
+        // 저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalArgumentException("예외 발생");
+        }
+        sleep(1000); // 상품을 저장하는데 걸리는 시간 1초
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
